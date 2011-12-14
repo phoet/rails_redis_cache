6c261f4d8b4e348fabe1f9f8f21cad0951239cc0
Peter Schröder <phoetmail@googlemail.com>
2011-12-14 16:20:59 +0100

    use rspec and apply gem best practices

--- rakefile.rb
+++ rakefile.rb
@@ -1,24 +1,17 @@
-require 'rake/testtask'
+require "bundler"
+require "rspec/core/rake_task"
 require "rake/rdoctask"
-require "rake/gempackagetask"
 
-spec = eval(File.new("rails_redis_cache.gemspec").readlines.join("\n"))
-
-Rake::GemPackageTask.new(spec) do |pkg|
-  pkg.need_zip = true
-  pkg.need_tar = true
-end
+Bundler::GemHelper.install_tasks
 
 Rake::RDocTask.new(:rdoc_dev) do |rd|
   rd.rdoc_files.include("lib/**/*.rb", "README.rdoc")
   rd.options + ['-a', '--inline-source', '--charset=UTF-8']
 end
 
-Rake::TestTask.new do |t|
-  t.libs << "test"
-  t.ruby_opts << "-rubygems"
-  t.test_files = FileList['test/test_*.rb']
-  t.verbose = true
+RSpec::Core::RakeTask.new do |t|
+  t.rspec_opts = ["--format Fuubar", "--color"]
+  t.pattern = 'spec/**/*_spec.rb'
 end
 
-task :default => :test
+task :default=>:spec
